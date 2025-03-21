05297ecb4488b239447088873e562425e206e475
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-03-21 12:34:31 +0530

    Update 23. Subsets.py

--- 23. Subsets.py
+++ 23. Subsets.py
@@ -1,27 +1,27 @@
 
 
-def subsetSum(num):
+def subset(num):
     # Write your code here.
 
-    def subsetSumFun(num,subset,ans,i):
+    def subsetFun(num,subset1,ans,i):
         if i==len(num):
             #append as list
-            ans.append(list(subset))
+            ans.append(list(subset1))
             #ans is list of list
             return 
             
         #Recursion
-        subset.append(num[i]) #choosing the element
-        subsetSumFun(num,subset,ans,i+1)
+        subset1.append(num[i]) #choosing the element
+        subsetFun(num,subset1,ans,i+1)
         #Backtracking
-        subset.pop() #not choosing the element
-        subsetSumFun(num,subset,ans,i+1)
+        subset1.pop() #not choosing the element
+        subsetFun(num,subset1,ans,i+1)
         return ans
         
     ans=[]
-    subset=[]
-    return subsetSumFun(num,subset,ans,0)
+    subset1=[]
+    return subsetFun(num,subset1,ans,0)
     
-ans=subsetSum([1,2,3])
+ans=subset([1,2,3])
 for i in ans:
     print(i)
